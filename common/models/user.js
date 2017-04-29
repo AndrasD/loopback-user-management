bd45639b902df7250e7c4a1b8b2571c300f40981
HOME-WIN10\Andras <andras.dallos@gmail.com>
2017-04-29 12:20:18 +0200

    sendGriddel megy a SignUp, Login, PasswordReset

--- common/models/user.js
+++ common/models/user.js
@@ -6,56 +6,51 @@
 var config = require('../../server/config.json');
 var path = require('path');
 
-module.exports = function (User) {
+module.exports = function(User) {
   //send verification email after registration
-  User.afterRemote('create', function (context, user, next) {
+  User.afterRemote('create', function(context, user, next) {
     console.log('> user.afterRemote triggered');
 
-    // using SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    var helper = require('sendgrid').mail;
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: 'noreply@sixis.hu',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
 
-    from_email = new helper.Email("sixis@sixis.hu");
-    to_email = new helper.Email(user.email);
-    subject = "Thanks for registering!";
-    content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
-    mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON()
-    });
-
-    sg.API(request, function (error, response) {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
+    user.verify(options, function(err, response) {
+      if (err) {
+        User.deleteById(user.id);
+        return next(err);
+      }
 
       console.log('> verification email sent:', response);
 
       context.res.render('response', {
         title: 'Signed up successfully',
-        content: 'Please check your email and click on the verification link before logging in.',
+        content: 'Please check your email and click on the verification link ' +
+            'before logging in.',
         redirectTo: '/',
         redirectToLinkText: 'Log in'
       });
     });
-
   });
 
   //send password reset link when requested
-  User.on('resetPasswordRequest', function (info) {
+  User.on('resetPasswordRequest', function(info) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
-    var html = 'Click <a href="' + url + '?access_token=' + info.accessToken.id + '">here</a> to reset your password';
+    var html = 'Click <a href="' + url + '?access_token=' +
+        info.accessToken.id + '">here</a> to reset your password';
 
     User.app.models.Email.send({
       to: info.email,
       from: info.email,
       subject: 'Password reset',
       html: html
-    }, function (err) {
+    }, function(err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
     });
